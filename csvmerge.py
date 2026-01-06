2a01c7fa925274616658fb34b6bbd9c605950762
ClaudioMartino <claudiomartino.it@gmail.com>
2026-01-06 17:04:19 +0100

    Adding skip options

--- csvmerge.py
+++ csvmerge.py
@@ -15,70 +15,106 @@ def print_highlighted_cell(row, i_to_highlight, header):
     print("")
 
 
-def csvmerge(inputfile1_path, inputfile2_path, outputfile_path, always=0):
-    with open(inputfile1_path, mode="r", encoding="utf-8") as inputfile1, \
-         open(inputfile2_path, mode="r", encoding="utf-8") as inputfile2, \
-         open(outputfile_path, mode="w", encoding="utf-8") as outputfile:
+def get_indices_cols_to_skip(col_names, header):
+    col_to_skip_indices = []
+    if col_names:
+        for col_to_skip in col_names:
+            try:
+                col_index = header.index(col_to_skip)
+            except ValueError:
+                print(f"'{col_to_skip}' doesn't exist in file 1")
+                continue
+            col_to_skip_indices.append(col_index)
+    col_to_skip_indices.sort()
+    return col_to_skip_indices
 
-        reader1 = csv.reader(inputfile1)
-        reader2 = csv.reader(inputfile2)
-        writer = csv.writer(outputfile)
+
+def csvmerge(in1_path, in2_path, out_path, always=0, skip1=[], skip2=[]):
+    with open(in1_path, mode="r", newline="", encoding="utf-8") as infile1, \
+         open(in2_path, mode="r", newline="", encoding="utf-8") as infile2, \
+         open(out_path, mode="w", newline="", encoding="utf-8") as outfile:
+
+        reader1 = csv.reader(infile1)
+        reader2 = csv.reader(infile2)
+        writer = csv.writer(outfile, lineterminator='\n')
+
+        # Look for indices of the columns to skip
+        header1 = next(reader1)
+        col_to_skip_indices1 = get_indices_cols_to_skip(skip1, header1)
+        header2 = next(reader2)
+        col_to_skip_indices2 = get_indices_cols_to_skip(skip2, header2)
 
         # Count total number of rows
-        tot_rows = 0
+        tot_rows = 1
         for row1, row2 in zip(reader1, reader2):
             tot_rows += 1
-        inputfile1.seek(0)
-        inputfile2.seek(0)
-        reader1 = csv.reader(inputfile1)
-        reader2 = csv.reader(inputfile2)
+        infile1.seek(0)
+        reader1 = csv.reader(infile1)
+        infile2.seek(0)
+        reader2 = csv.reader(infile2)
 
         i_row = 0
         for row1, row2 in zip_longest(reader1, reader2):
+            # Remove skipped columns
+            row1_skip = []
+            if row1:
+                for offset, i in enumerate(col_to_skip_indices1):
+                    row1_skip.append(row1.pop(i - offset))
+            row2_skip = []
+            if row2:
+                for offset, i in enumerate(col_to_skip_indices2):
+                    row2_skip.append(row2.pop(i - offset))
+
             output_row = []
             if row1 and row2:
-                for (i_col, c1), c2 in zip(enumerate(row1), row2):
-                    if c1 != c2:
-                        if always:
-                            if always == 1:
-                                output_row.append(c1)
-                            if always == 2:
-                                output_row.append(c2)
-                        else:
-                            while True:
-                                print(f"--- Row {i_row + 1} of {tot_rows}, \
-Column {i_col + 1} ---")
-                                print_highlighted_cell(row1, i_col, "[1]")
-                                print_highlighted_cell(row2, i_col, "[2]")
-                                user_input = input("> ")
-                                if user_input == "1":
+                i_col = 0
+                for c1, c2 in zip_longest(row1, row2):
+                    if c1 and c2:
+                        if c1 != c2:
+                            if always:
+                                if always == 1:
                                     output_row.append(c1)
-                                    break
-                                elif user_input == "2":
+                                if always == 2:
                                     output_row.append(c2)
-                                    break
-                                elif user_input == "q":
-                                    return
-                                else:
-                                    print("Invalid input. Type 1 or 2 to \
+                            else:
+                                while True:
+                                    print(f"--- Row {i_row + 1} of {tot_rows}\
+, Column {i_col + 1} ---")
+                                    print_highlighted_cell(row1, i_col, "[1]")
+                                    print_highlighted_cell(row2, i_col, "[2]")
+                                    user_input = input("> ")
+                                    if user_input == "1":
+                                        output_row.append(c1)
+                                        break
+                                    elif user_input == "2":
+                                        output_row.append(c2)
+                                        break
+                                    elif user_input == "q":
+                                        return
+                                    else:
+                                        print("Invalid input. Type 1 or 2 to \
 select the file, q to exit.")
+                        else:
+                            output_row.append(c1)  # it's equal to c2
                     else:
-                        output_row.append(c1)
-
-                # Handle tail for rows with different number of columns
-                if len(row1) > len(row2):
-                    print(f"Adding columns {len(row2)+1} to {len(row1)} from \
-file 1")
-                    output_row.extend(row1[len(row2):])
-                if len(row2) > len(row1):
-                    print(f"Adding columns {len(row1)+1} to {len(row2)} from \
-file 2")
-                    output_row.extend(row2[len(row1):])
+                        if c1:
+                            output_row.append(c1)
+                        if c2:
+                            output_row.append(c2)
+                    i_col += 1
             else:
                 if row1:
                     output_row.extend(row1)
                 if row2:
                     output_row.extend(row2)
+
+            # Add skipped columns on the right
+            if row1_skip:
+                output_row.extend(row1_skip)
+            if row2_skip:
+                output_row.extend(row2_skip)
+
+            # Write row in output file
             writer.writerow(output_row)
             i_row += 1
 
@@ -95,8 +131,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "--always", type=int, default=0, choices=[1, 2],
         help="File from which always pick")
+    parser.add_argument(
+        "--skip1", metavar="cn", nargs="+", help="Names of the columns to \
+remove from file #1 before the comparison. The columns are added back to the \
+output file, to the right")
+    parser.add_argument(
+        "--skip2", metavar="cn", nargs="+", help="Names of the columns to \
+remove from file #2 before the comparison. The columns are added back to the \
+output file, to the right")
     parser_args = vars(parser.parse_args())
 
     csvmerge(
         parser_args["i1"], parser_args["i2"], parser_args["o"],
-        parser_args["always"])
+        parser_args["always"], parser_args["skip1"], parser_args["skip2"])
--- testing/test.py
+++ testing/test.py
@@ -4,72 +4,135 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import csvmerge  # import ../csvmerge.py
 
 
-def test(i1, i2, ref=None, always=0):
-    # Run function
+def test(i1, i2, ref=None, always=0, skip1=[], skip2=[]):
+    error = False
     tmp_output = "out.csv"
-    csvmerge.csvmerge(i1, i2, tmp_output, always)
+
+    # Run function
+    csvmerge.csvmerge(i1, i2, tmp_output, always, skip1, skip2)
 
     # Check reference file
     if ref:
-        error = False
         with open(tmp_output) as outputfile:
             with open(ref) as referencefile:
                 if outputfile.read() != referencefile.read():
                     error = True
-        os.remove(tmp_output)
-        if error:
-            raise Exception(f"Error! {i1} vs {i2} != {ref}")
+
+    # Remove output file
+    os.remove(tmp_output)
+
+    # Raise exception if error
+    if error:
+        raise Exception(f"Error! {i1} vs {i2} != {ref}")
 
 
 # TODO files with empty cells
+
+# - Always x 3
+# -- Table dimensions x 4
+# --- Content x 2
+# ---- Skip columns x 4
+
 def main():
     # - No always option (same content only, no decision taken)
     # -- Same size
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "3r_3c.csv", "3r_3c.csv")
+    # ---- Skip same columns
+    test("3r_3c.csv", "3r_3c.csv", "3r_3c_skip2_3r_3c_skip2.csv", 0, ['header2'], ['header2'])
     # -- Different number of rows (check rows tail)
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "5r_3c.csv", "5r_3c.csv")
     test("5r_3c.csv", "3r_3c.csv", "5r_3c.csv")
     # -- Different number of columns (check cols tail)
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "3r_5c.csv", "3r_5c.csv")
     test("3r_5c.csv", "3r_3c.csv", "3r_5c.csv")
+    # ---- Skip same columns
+    test("3r_3c.csv", "3r_5c.csv", "3r_3c_skip2_3r_5c_skip2.csv", 0, ['header2'], ['header2'])
+    test("3r_5c.csv", "3r_3c.csv", "3r_5c_skip2_3r_3c_skip2.csv", 0, ['header2'], ['header2'])
+    # ---- Skip different columns (columns not in common)
+    test("3r_3c.csv", "3r_5c.csv", "3r_5c.csv", 0, [], ['header5'])
+    test("3r_3c.csv", "3r_5c.csv", "3r_5c.csv", 0, [], ['header5', 'header4'])
     # -- Different number of rows and columns (check both tails)
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "5r_5c.csv", "5r_5c.csv")
     test("5r_5c.csv", "3r_3c.csv", "5r_5c.csv")
 
     # - Always=1
     # -- Same size
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "3r_3c.csv", "3r_3c.csv", 1)
+    # ---- Skip same columns
+    test("3r_3c.csv", "3r_3c.csv", "3r_3c_skip2_3r_3c_skip2.csv", 1, ['header2'], ['header2'])
+    # ---- Skip different columns
+    test("3r_3c.csv", "3r_3c.csv", "3r_3c_skip1_3r_3c_skip3.csv", 1, ['header1'], ['header3'])
     # --- Different content
+    # ---- Don't skip
     test("3r_3c_err.csv", "3r_3c.csv",     "3r_3c_err.csv", 1)
     test("3r_3c.csv",     "3r_3c_err.csv", "3r_3c.csv",     1)
+    # ---- Skip same columns
+    test("3r_3c_err.csv", "3r_3c.csv",     "3r_3c_err_skip2_3r_3c_skip2.csv", 1, ['header2'], ['header2'])
+    test("3r_3c.csv",     "3r_3c_err.csv", "3r_3c_skip2_3r_3c_err_skip2.csv", 1, ['header2'], ['header2'])
+    # ---- Skip different columns
+    test("3r_3c_err.csv", "3r_3c.csv",     "3r_3c_err_skip1_3r_3c_skip3.csv", 1, ['header1'], ['header3'])
+    test("3r_3c.csv",     "3r_3c_err.csv", "3r_3c_skip1_3r_3c_err_skip3.csv", 1, ['header1'], ['header3'])
     # -- Different number of rows (check rows tail)
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "5r_3c.csv", "5r_3c.csv", 1)
     test("5r_3c.csv", "3r_3c.csv", "5r_3c.csv", 1)
     # --- Different content
+    # ---- Don't skip
     test("3r_3c_err.csv", "5r_3c.csv",     "5r_3c_err2.csv", 1)
     test("3r_3c.csv",     "5r_3c_err.csv", "5r_3c_err3.csv", 1)
     test("5r_3c_err.csv", "3r_3c.csv",     "5r_3c_err.csv",  1)
     test("5r_3c.csv",     "3r_3c_err.csv", "5r_3c.csv",      1)
     # -- Different number of columns (check cols tail)
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "3r_5c.csv", "3r_5c.csv", 1)
     test("3r_5c.csv", "3r_3c.csv", "3r_5c.csv", 1)
+    # ---- Skip same columns
+    test("3r_3c.csv", "3r_5c.csv", "3r_3c_skip2_3r_5c_skip2.csv", 1, ['header2'], ['header2'])
+    test("3r_5c.csv", "3r_3c.csv", "3r_3c_skip2_3r_5c_skip2.csv", 1, ['header2'], ['header2'])
+    # ---- Skip different columns (columns in common)
+    test("3r_3c.csv", "3r_5c.csv", "3r_3c_skip1_3r_5c_skip3.csv", 1, ['header1'], ['header3'])
+    test("3r_5c.csv", "3r_3c.csv", "3r_5c_skip1_3r_3c_skip3.csv", 1, ['header1'], ['header3'])
+    # ---- Skip different columns (columns not in common)
+    test("3r_5c.csv", "3r_3c.csv", "3r_5c_skip4_3r_3c_skip3.csv", 1, ['header4'], ['header3'])
     # --- Different content
+    # ---- Don't skip
     test("3r_3c_err.csv", "3r_5c.csv",     "3r_5c_err2.csv", 1)
     test("3r_3c.csv",     "3r_5c_err.csv", "3r_5c_err3.csv", 1)
     test("3r_5c_err.csv", "3r_3c.csv",     "3r_5c_err.csv",  1)
     test("3r_5c.csv",     "3r_3c_err.csv", "3r_5c.csv",      1)
+    # ---- Skip same columns
+    test("3r_3c_err.csv", "3r_5c.csv",     "3r_3c_err_skip2_3r_5c_skip2.csv", 1, ['header2'], ['header2'])
+    test("3r_3c.csv",     "3r_5c_err.csv", "3r_3c_skip2_3r_5c_err_skip2.csv", 1, ['header2'], ['header2'])
+    test("3r_5c_err.csv", "3r_3c.csv",     "3r_5c_err_skip2_3r_3c_skip2.csv", 1, ['header2'], ['header2'])
+    test("3r_5c.csv",     "3r_3c_err.csv", "3r_5c_skip2_3r_3c_err_skip2.csv", 1, ['header2'], ['header2'])
+    # ---- Skip different columns (columns in common)
+    test("3r_3c_err.csv", "3r_5c.csv",     "3r_3c_err_skip1_3r_5c_skip3.csv", 1, ['header1'], ['header3'])
+    test("3r_3c.csv",     "3r_5c_err.csv", "3r_3c_skip1_3r_5c_err_skip3.csv", 1, ['header1'], ['header3'])
+    test("3r_5c_err.csv", "3r_3c.csv",     "3r_5c_err_skip1_3r_3c_skip3.csv", 1, ['header1'], ['header3'])
+    test("3r_5c.csv",     "3r_3c_err.csv", "3r_5c_skip1_3r_3c_err_skip3.csv", 1, ['header1'], ['header3'])
+    # ---- Skip different columns (columns not in common)
+    test("3r_5c_err.csv", "3r_3c.csv",     "3r_5c_err_skip4_3r_3c_skip3.csv", 1, ['header4'], ['header3'])
+    test("3r_5c.csv",     "3r_3c_err.csv", "3r_5c_skip4_3r_3c_err_skip3.csv", 1, ['header4'], ['header3'])
+
     # -- Different number of rows and columns (check both tails)
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "5r_5c.csv", "5r_5c.csv", 1)
     test("5r_5c.csv", "3r_3c.csv", "5r_5c.csv", 1)
     # --- Different content
+    # ---- Don't skip
     test("3r_3c_err.csv", "5r_5c.csv",     "5r_5c_err2.csv", 1)
     test("3r_3c.csv",     "5r_5c_err.csv", "5r_5c_err3.csv", 1)
     test("5r_5c_err.csv", "3r_3c.csv",     "5r_5c_err.csv",  1)
@@ -78,33 +141,41 @@ def main():
     # - Always=2
     # -- Same size
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "3r_3c.csv", "3r_3c.csv", 2)
     # --- Different content
+    # ---- Don't skip
     test("3r_3c_err.csv", "3r_3c.csv",     "3r_3c.csv",     2)
     test("3r_3c.csv",     "3r_3c_err.csv", "3r_3c_err.csv", 2)
     # -- Different number of rows (check rows tail)
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "5r_3c.csv", "5r_3c.csv", 2)
     test("5r_3c.csv", "3r_3c.csv", "5r_3c.csv", 2)
     # --- Different content
+    # ---- Don't skip
     test("3r_3c_err.csv", "5r_3c.csv",     "5r_3c.csv",      2)
     test("3r_3c.csv",     "5r_3c_err.csv", "5r_3c_err.csv",  2)
     test("5r_3c_err.csv", "3r_3c.csv",     "5r_3c_err3.csv", 2)
     test("5r_3c.csv",     "3r_3c_err.csv", "5r_3c_err2.csv", 2)
     # -- Different number of columns (check cols tail)
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "3r_5c.csv", "3r_5c.csv", 2)
     test("3r_5c.csv", "3r_3c.csv", "3r_5c.csv", 2)
     # --- Different content
+    # ---- Don't skip
     test("3r_3c_err.csv", "3r_5c.csv",     "3r_5c.csv",      2)
     test("3r_3c.csv",     "3r_5c_err.csv", "3r_5c_err.csv",  2)
     test("3r_5c_err.csv", "3r_3c.csv",     "3r_5c_err3.csv", 2)
     test("3r_5c.csv",     "3r_3c_err.csv", "3r_5c_err2.csv", 2)
     # -- Different number of rows and columns (check both tails)
     # --- Same content
+    # ---- Don't skip
     test("3r_3c.csv", "5r_5c.csv", "5r_5c.csv", 2)
     test("5r_5c.csv", "3r_3c.csv", "5r_5c.csv", 2)
     # --- Different content
+    # ---- Don't skip
     test("3r_3c_err.csv", "5r_5c.csv",     "5r_5c.csv",      2)
     test("3r_3c.csv",     "5r_5c_err.csv", "5r_5c_err.csv",  2)
     test("5r_5c_err.csv", "3r_3c.csv",     "5r_5c_err3.csv", 2)
